84c74c0f5349961aff918886ed8f817b61f723a5
Lucas Souza <outrolucas2002@gmail.com>
2022-08-07 22:42:29 -0300

    add getters and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,69 @@ export default new Vuex.Store({
   state: {
     people: [],
   },
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    filterPeople: (state) => (search) => {
+      var cleanPeople = [];
+      state.people.forEach((person) => {
+        let filteredPerson = {
+          completeName: "",
+          gender: "",
+          dob: "",
+          id: "",
+          url: "",
+          location: "",
+          email: "",
+          phone: "",
+          nat: "",
+          picture: "",
+        };
+
+        filteredPerson[
+          "completeName"
+        ] = `${person.name.first} ${person.name.last}`;
+        console.log(filteredPerson);
+        filteredPerson["gender"] = person.gender;
+        filteredPerson["dob"] = person.dob.date.split("T")[0];
+        filteredPerson["id"] = person.login.username;
+        filteredPerson[
+          "location"
+        ] = `${person.location.street.name} ${person.location.street.number}, ${person.location.postcode}, ${person.location.city} - ${person.location.state}, ${person.location.country}`;
+        filteredPerson["email"] = person.login.email;
+        filteredPerson["phone"] = person.login.phone;
+        filteredPerson["nat"] = person.login.nat;
+        filteredPerson["picture"] = person.picture.medium;
+
+        console.log(filteredPerson);
+        cleanPeople.push(filteredPerson);
+      });
+
+      function compare(a, b) {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+
+        return 0;
+      }
+
+      let filteredPeople = cleanPeople.filter((person) => {
+        return (
+          person.completeName.toLowerCase().indexOf(search.toLowerCase()) != -1
+        );
+      });
+
+      filteredPeople.sort(compare);
+
+      return state.people;
+    },
+  },
+  mutations: {
+    addPeople(state, payload) {
+      state.people.push.apply(state.people, payload.newPeople);
+    },
+  },
+  actions: {
+    loadPeople(context) {
+      
+    }
+  },
   modules: {},
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,7 @@
       </thead>
       <thead>
         <Person
-          v-for="person in filteredPeople"
+          v-for="person in people"
           :key="person.login.username"
           :personInfo="person"
         />
@@ -46,18 +46,90 @@ export default {
     };
   },
   methods: {
-    api: function (page) {
-      fetch(
-        `https://randomuser.me/api/?page=${page}&results=50&seed=easterEgg`
-      ).then((res) =>
-        res
-          .json()
-          .then((json) =>
-            this.$store.state.people.push.apply(this.people, json.results)
-          )
-      );
+    api() {
+      let list = [
+        {
+          gender: "female",
+          name: { title: "Miss", first: "Elfi", last: "Heinzel" },
+          location: {
+            street: { number: 1520, name: "Mühlenstraße" },
+            city: "Pohlheim",
+            state: "Baden-Württemberg",
+            country: "Germany",
+            postcode: 58633,
+            coordinates: { latitude: "-52.7998", longitude: "90.7987" },
+            timezone: {
+              offset: "+4:00",
+              description: "Abu Dhabi, Muscat, Baku, Tbilisi",
+            },
+          },
+          email: "elfi.heinzel@example.com",
+          login: {
+            uuid: "45092bb0-8d49-40e2-ad60-20ce405ee277",
+            username: "beautifuldog141",
+            password: "griffin",
+            salt: "YpLb4i32",
+            md5: "160edc64834ef8a3bdc80300e44359fe",
+            sha1: "6b462f087fbe491d7318b0d97b50cf195429def1",
+            sha256:
+              "c1d7738c75eafe2b823a45a894bd300db6060bbc537856d36cf537d77af56631",
+          },
+          dob: { date: "1990-03-20T06:03:27.939Z", age: 32 },
+          registered: { date: "2021-01-14T22:34:51.663Z", age: 1 },
+          phone: "0038-8144796",
+          cell: "0172-5948812",
+          id: { name: "SVNR", value: "38 200390 H 895" },
+          picture: {
+            large: "https://randomuser.me/api/portraits/women/29.jpg",
+            medium: "https://randomuser.me/api/portraits/med/women/29.jpg",
+            thumbnail: "https://randomuser.me/api/portraits/thumb/women/29.jpg",
+          },
+          nat: "DE",
+        },
+        {
+          gender: "female",
+          name: { title: "Ms", first: "Gordana", last: "Stojković" },
+          location: {
+            street: { number: 1396, name: "Milovana Milenkovića" },
+            city: "Čoka",
+            state: "Prizren",
+            country: "Serbia",
+            postcode: 15080,
+            coordinates: { latitude: "38.4004", longitude: "156.4846" },
+            timezone: {
+              offset: "-1:00",
+              description: "Azores, Cape Verde Islands",
+            },
+          },
+          email: "gordana.stojkovic@example.com",
+          login: {
+            uuid: "a38e7ed1-45c9-4db4-8d62-83a0cd6f55e0",
+            username: "whiteladybug154",
+            password: "skidoo",
+            salt: "U1A7ux6b",
+            md5: "de15706d9f697074ddca88dcf0b6fa90",
+            sha1: "988d311d9c4d06ceaf318863df8f84bd5c754c99",
+            sha256:
+              "c6c8e5891fd458e9c9bcc50bb1df7996d9bd07fc499cfd33926920f62488e0c4",
+          },
+          dob: { date: "2000-02-20T03:34:32.266Z", age: 22 },
+          registered: { date: "2016-07-11T09:17:51.631Z", age: 6 },
+          phone: "036-1381-395",
+          cell: "065-0999-098",
+          id: { name: "SID", value: "594793293" },
+          picture: {
+            large: "https://randomuser.me/api/portraits/women/72.jpg",
+            medium: "https://randomuser.me/api/portraits/med/women/72.jpg",
+            thumbnail: "https://randomuser.me/api/portraits/thumb/women/72.jpg",
+          },
+          nat: "RS",
+        },
+      ];
 
-      this.nextPage = this.nextPage + 1;
+      this.$store.commit({
+        type: "addPeople",
+        newPeople: list,
+      });
     },
   },
   mounted() {
@@ -65,27 +137,7 @@ export default {
   },
   computed: {
     people() {
-      return this.$store.state.people;
-    },
-    filteredPeople() {
-      function compare(a, b) {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-
-        return 0;
-      }
-
-      let people = this.$store.state.people.filter((person) => {
-        return (
-          person.name.first
-            .toLowerCase()
-            .indexOf(this.searchNameTextValue.toLowerCase()) != -1
-        );
-      });
-
-      people.sort(compare);
-
-      return people;
+      return this.$store.getters.filterPeople(this.searchNameTextValue);
     },
   },
 };
